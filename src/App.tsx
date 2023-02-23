85284ca9b3e576232e4f1cebb9480930ee95432e
Muhammad Saad <mommo142018@gmail.com>
2023-02-23 10:22:25 +0100

    with comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import "./App.css";
 import Navbar from "./Components/Nav";
 import MainArea from "./Components/MainArea";
 
+// Defining the main app component
+
 function App() {
+  // Returning JSX markup for the component's render output
     return (
+      // Wrapping the Navbar and MainArea components in a parent div element
       <div>
         <Navbar></Navbar>
   
@@ -15,4 +19,5 @@ function App() {
     );
   }
   
+  // Exporting the App component as the default export of this file
   export default App;
