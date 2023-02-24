016e9229e9448ff3d68322ab710f0e7a0af6db16
Olof Bexelius <olof@student-218-47.eduroam.uu.se>
2023-02-24 15:38:14 +0100

    Saving to local storage

--- src/Components/MainArea.tsx
+++ src/Components/MainArea.tsx
@@ -33,3 +33,96 @@ export default function MainArea() {
     // Update the state
     loadTasks();
   };
+
+   const handleUpdateTask = (taskName: ITask): void => {
+    // Get the existing tasks from local storage, if any
+    const storedTasks = localStorage.getItem("tasks");
+    const tasks = storedTasks ? JSON.parse(storedTasks) : [];
+
+    // Delete the task from the array
+    const newTaskList = tasks.filter(
+      (t: { TaskID: string }) => t.TaskID !== taskName.TaskID
+    );
+    // Add the updated task to the array
+    newTaskList.push(taskName);
+
+    localStorage.setItem("tasks", JSON.stringify(newTaskList));
+    loadTasks();
+  };
+
+  const loadTasks = () => {
+    const storedTasks = localStorage.getItem("tasks");
+    const tasksArray = storedTasks ? JSON.parse(storedTasks) : [];
+
+    // Create a new instance of the HashTable class to store the tasks
+    const taskMap = new HashTable<string, ITask>(tasksArray.length);
+
+    // Add each task to the hash map with its task ID as the key
+    tasksArray.forEach((task: ITask) => {
+      taskMap.set(task.TaskID, task);
+    });
+
+    // Create a new type using the KeyValuePair class
+    type TaskKeyValuePair = KeyValuePair<string, ITask>;
+
+    const priorityOrder: string[] = ["yellow", "blue", "red"];
+
+    // Create a new instance of the Stack class to store the prioritized tasks
+    const prioritizedTaskStack = new Stack<ITask>();
+
+    priorityOrder.forEach((priority: string) => {
+      // Get all the tasks with the given priority
+      const allTasks = taskMap.getAllEntries();
+      allTasks.forEach((task: TaskKeyValuePair) => {
+        if (task.value.TaskPriority === priority) {
+          prioritizedTaskStack.push(task.value);
+        }
+      });
+    });
+    // Pop the tasks from the stack to create the prioritized task list
+    const prioritizedTaskList: ITask[] = [];
+    while (!prioritizedTaskStack.isEmpty()) {
+      const task = prioritizedTaskStack.pop();
+      if (task) {
+        prioritizedTaskList.push(task);
+      }
+    }
+
+    setListTask(prioritizedTaskList);
+    console.log("Prioritized Task List 2", prioritizedTaskList);
+  };
+
+  const loadData = () => {
+    loadTasks();
+  };
+
+  useEffect(() => {
+    loadData();
+    console.log("useEffect is called");
+  }, []);
+
+  // TODO : Conditional rendering based on one
+
+  return (
+    <>
+      <div className="main-area">
+        <AddTask onAddTask={handleAddTask}></AddTask>
+        <div className="task-list">
+          {ListTask.length !== 0 ? (
+            <h1>Task List</h1>
+          ) : (
+            <h1 className="heading2">No Task Added</h1>
+          )}
+          {ListTask.map((task, key: number) => (
+            <TodoTask
+              task={task}
+              key={task.TaskID}
+              handleDeletetask={handleDeleteTask}
+              handleUpdateTask={handleUpdateTask}
+            ></TodoTask>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
