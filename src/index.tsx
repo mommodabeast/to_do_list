f6a8433ebe2ad1edc81cd96d531199a8d0dc5b3e
Muhammad Saad <mommo142018@gmail.com>
2023-02-27 13:09:10 +0100

    with comments

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// Creating a new root for the React application using the createRoot() method
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// Rendering the App component into the newly created root using the render() method
 root.render(<App />);
 
-
+// reporting the web vitals
 reportWebVitals();
